109711a988e6b14dc786a0203b58770ffd0064e2
schmidgeri <schmid.geri@bluewin.ch>
2024-02-02 15:44:16 +0100

    SDS-9999 Kurs Mergebasis

---
+++ MitarbeiterExtern.sql
@@ -0,0 +1,15 @@
+SELECT *
+FROM Mitarbeiter
+WHERE Gehalt > 50000;
+
+SELECT *
+FROM Mitarbeiter
+WHERE Spesen > 5000;
+
+SELECT *
+FROM Mitarbeiter
+WHERE Ueberzeit > 50;
+
+SELECT *
+FROM Mitarbeiter
+WHERE FerienTage > 50;
