33218e44c011a6615c13d27f288b48801c415d33
schmidgeri <schmid.geri@bluewin.ch>
2024-02-06 12:32:51 +0100

    SDS-9999 KursMerge

---
+++ partner1.sql
@@ -0,0 +1,7 @@
+select * from mitarbeier wehere lohn > 5000;
+
+select * from mitarbeier wehere ferien > 15;
+
+select * from mitarbeier wehere spesen > 3000;
+
+select * from mitarbeier wehere absenzen > 15;
