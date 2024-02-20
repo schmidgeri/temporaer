62f26873f60556c96cbb89f2e17e320d06e27cfa
Tobias Kusmitsch <tobias@kusmitsch.net>
2024-02-20 15:39:32 +0100

    SDS-112233 ErsterCommit

---
+++ mergeTobias.sql
@@ -0,0 +1,12 @@
+SELECT *
+FROM Mitarbeiter
+WHERE Gehalt > 50001; 
+SELECT *
+FROM Mitarbeiter
+WHERE Spesen > 5004; 
+SELECT *
+FROM Mitarbeiter
+WHERE Ueberzeit > 50;
+SELECT *
+FROM Mitarbeiter
+WHERE FerienTage > 51;  
