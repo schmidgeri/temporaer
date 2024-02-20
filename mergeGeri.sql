404d3207fe9e4ae7a7072d06a5faf466c17d7cac
schmidgeri <schmid.geri@bluewin.ch>
2024-02-20 15:15:18 +0100

    SDS-9999 Meerge Geri Team B

--- mergeGeri.sql
+++ mergeGeri.sql
@@ -4,7 +4,22 @@ WHERE Gehalt > 50001;
 
 SELECT * 
 FROM Mitarbeiter
-WHERE Spesen > 5004;  
+WHERE Spesen in ( 5004, 5050);  
+
+SELECT *
+FROM Mitarbeiter
+WHERE Ueberzeit > 50;
+
+SELECT *
+FROM Mitarbeiter
+WHERE FerienTage > 51; 
+SELECT *
+FROM Mitarbeiter
+WHERE Gehalt > 50001;  
+
+SELECT *
+FROM Mitarbeiter
+WHERE Spesen > 5050  
 
 SELECT *
 FROM Mitarbeiter
